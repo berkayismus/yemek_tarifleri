ff8baf5e78b33b252b8ade1d0417b88fefbe72cf
berkay.cayli <berkaydiyebiri@gmail.com>
2026-07-14 14:41:39 +0300

    web url'den # işareti kaldırıldı (PathUrlStrategy)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'i18n/strings.g.dart';
@@ -7,6 +8,10 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    setUrlStrategy(PathUrlStrategy());
+  }
 
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
