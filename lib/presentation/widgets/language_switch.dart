b7f7c2c37d8fcbfda593ba0ae65fb9ba93441e7a
berkay.cayli <berkaydiyebiri@gmail.com>
2026-07-15 14:48:44 +0300

    dil değiştirme butonu StatefulWidget yapıldı, SharedPreferences cached

--- lib/presentation/widgets/language_switch.dart
+++ lib/presentation/widgets/language_switch.dart
@@ -2,8 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../i18n/strings.g.dart';
 
-class LanguageSwitch extends StatelessWidget {
+class LanguageSwitch extends StatefulWidget {
   const LanguageSwitch({super.key});
+
+  @override
+  State<LanguageSwitch> createState() => _LanguageSwitchState();
+}
+
+class _LanguageSwitchState extends State<LanguageSwitch> {
+  SharedPreferences? _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() => _prefs = prefs);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +28,8 @@ class LanguageSwitch extends StatelessWidget {
       tooltip: current == AppLocale.tr ? 'English' : 'Türkçe',
       onPressed: () async {
         final newLocale = current == AppLocale.tr ? AppLocale.en : AppLocale.tr;
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setString('app_locale', newLocale.languageCode);
         await LocaleSettings.setLocale(newLocale);
+        _prefs?.setString('app_locale', newLocale.languageCode);
       },
     );
   }
