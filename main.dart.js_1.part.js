1fb691d9fff4ab5643c3d1259d60f90c5b4bd85b
berkayismus <berkayismus@users.noreply.github.com>
2026-07-15 12:27:59 +0000

    deploy: 4d6abf17b0b07a2231881a28221ee509bb89407f

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,242 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
+aU_(d,e,f){var w=new B.oH(C.kQ,d,x.c),v=new A.Xd(w)
+w.r=A.b9m(v)
+return v},
+b9k(d,e){var w,v="Categories",u="New Recipe",t="Delete Recipe",s="Ingredients",r="Instructions"
+A:{w="Discover"
+if("appTitle"===e){w="Cookmark"
+break A}if("nav.discover"===e)break A
+if("nav.saved"===e){w="Saved"
+break A}if("discover.title"===e)break A
+if("discover.searchHint"===e){w="Search recipes in API..."
+break A}if("discover.searchButton"===e){w="Search"
+break A}if("discover.noResults"===e){w=new A.aHA()
+break A}if("discover.searchPrompt"===e){w=y.o
+break A}if("discover.saved"===e){w=new A.aHB()
+break A}if("discover.alreadySaved"===e){w="This recipe is already saved"
+break A}if("discover.saveTooltip"===e){w="Save"
+break A}if("discover.fetchFailed"===e){w="Could not fetch recipe."
+break A}if("saved.title"===e){w="Saved Recipes"
+break A}if("saved.searchHint"===e){w="Search recipes..."
+break A}if("saved.noResults"===e){w="No recipes match your search."
+break A}if("saved.empty"===e){w="No saved recipes yet"
+break A}if("saved.emptyHint"===e){w="Use the Discover tab to search and save recipes"
+break A}if("saved.deleteAll"===e){w="Delete All"
+break A}if("saved.deleteAllTitle"===e){w="Delete All Recipes"
+break A}if("saved.deleteAllContent"===e){w=y.c
+break A}if("saved.randomRecipeTooltip"===e){w="Suggest Random Recipe"
+break A}if("saved.categoriesTooltip"===e){w=v
+break A}if("saved.newRecipeTooltip"===e){w=u
+break A}if("saved.noRecipe"===e){w="No recipes yet."
+break A}if("saved.deleteTitle"===e){w=t
+break A}if("saved.deleteContent"===e){w=new A.aHC()
+break A}if("common.cancel"===e){w="Cancel"
+break A}if("common.delete"===e){w="Delete"
+break A}if("recipeForm.editTitle"===e){w="Edit Recipe"
+break A}if("recipeForm.newTitle"===e){w="Add New Recipe"
+break A}if("recipeForm.nameLabel"===e){w="Recipe Name"
+break A}if("recipeForm.categoryLabel"===e){w="Category"
+break A}if("recipeForm.categoryHint"===e){w="e.g. Soup, Main Course, Dessert"
+break A}if("recipeForm.imageUrlLabel"===e){w="Image URL (optional)"
+break A}if("recipeForm.imageUrlHint"===e){w="https://..."
+break A}if("recipeForm.ingredientsLabel"===e){w=s
+break A}if("recipeForm.instructionsLabel"===e){w=r
+break A}if("recipeForm.updateButton"===e){w="Update"
+break A}if("recipeForm.saveButton"===e){w="Save"
+break A}if("recipeForm.nameRequired"===e){w="Recipe name is required"
+break A}if("recipeForm.categoryRequired"===e){w="Category is required"
+break A}if("recipeForm.ingredientsRequired"===e){w="Ingredients are required"
+break A}if("recipeForm.instructionsRequired"===e){w="Instructions are required"
+break A}if("categories.title"===e){w=v
+break A}if("categories.empty"===e){w="No categories yet"
+break A}if("categories.emptyHint"===e){w=y.d
+break A}if("categories.recipeCount"===e){w=new A.aHD(d)
+break A}if("categoryRecipes.empty"===e){w="No recipes in this category"
+break A}if("categoryRecipes.addRecipe"===e){w="Add Recipe"
+break A}if("categoryRecipes.newRecipeTooltip"===e){w=u
+break A}if("categoryRecipes.deleteTitle"===e){w=t
+break A}if("categoryRecipes.deleteContent"===e){w=new A.aHE()
+break A}if("recipeDetail.ingredients"===e){w=s
+break A}if("recipeDetail.instructions"===e){w=r
+break A}if("recipeDetail.notFound"===e){w="Recipe not found"
+break A}if("recipeDetail.share"===e){w="Share"
+break A}if("recipeDetail.shareSubject"===e){w=new A.aHF()
+break A}if("recipeDetail.copiedToClipboard"===e){w="Link copied to clipboard"
+break A}w=null
+break A}return w},
+b9m(d){return new A.aHH(d)},
+Xd:function Xd(d){var _=this
+_.a=d
+_.y=_.x=_.w=_.r=_.f=_.e=_.d=_.c=_.b=$},
+aGy:function aGy(){},
+aGx:function aGx(){},
+aGB:function aGB(){},
+aGw:function aGw(){},
+aGA:function aGA(){},
+aGu:function aGu(d){this.a=d},
+aGv:function aGv(){},
+aGz:function aGz(){},
+aHH:function aHH(d){this.a=d},
+aHA:function aHA(){},
+aHB:function aHB(){},
+aHC:function aHC(){},
+aHD:function aHD(d){this.a=d},
+aHE:function aHE(){},
+aHF:function aHF(){},
+a6K:function a6K(){}}
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[3],A)
+A.Xd.prototype={
+i(d,e){return this.a.NL(e)},
+gmd(){var w=this.b
+return w===$?this.b=this:w},
+gY0(){return"Cookmark"},
+gM3(){var w=this,v=w.c
+if(v===$){w.gmd()
+w.c!==$&&B.al()
+v=w.c=new A.aGy()}return v},
+gkC(){var w=this,v=w.d
+if(v===$){w.gmd()
+w.d!==$&&B.al()
+v=w.d=new A.aGx()}return v},
+ghk(){var w=this,v=w.e
+if(v===$){w.gmd()
+w.e!==$&&B.al()
+v=w.e=new A.aGB()}return v},
+gpt(){var w=this,v=w.f
+if(v===$){w.gmd()
+w.f!==$&&B.al()
+v=w.f=new A.aGw()}return v},
+ghM(){var w=this,v=w.r
+if(v===$){w.gmd()
+w.r!==$&&B.al()
+v=w.r=new A.aGA()}return v},
+gvS(){var w,v=this,u=v.w
+if(u===$){w=v.gmd()
+v.w!==$&&B.al()
+u=v.w=new A.aGu(w)}return u},
+gvT(){var w=this,v=w.x
+if(v===$){w.gmd()
+w.x!==$&&B.al()
+v=w.x=new A.aGv()}return v},
+gqd(){var w=this,v=w.y
+if(v===$){w.gmd()
+w.y!==$&&B.al()
+v=w.y=new A.aGz()}return v},
+$ifJ:1,
+gEk(){return this.a}}
+A.aGy.prototype={
+gkC(){return"Discover"},
+ghk(){return"Saved"}}
+A.aGx.prototype={
+ge_(){return"Discover"},
+gua(){return"Search recipes in API..."},
+gO0(){return"Search"},
+a1k(d){return"No results found for "+d+"."},
+gO1(){return y.o},
+NU(d){return d+" saved!"},
+gXY(){return"This recipe is already saved"}}
+A.aGB.prototype={
+ge_(){return"Saved Recipes"},
+gua(){return"Search recipes..."},
+ga1j(){return"No recipes match your search."},
+gnS(){return"No saved recipes yet"},
+gZj(){return"Delete All"},
+gZl(){return"Delete All Recipes"},
+gZk(){return y.c},
+ga1S(){return"Suggest Random Recipe"},
+gYz(){return"Categories"},
+ga1i(){return"No recipes yet."},
+gw7(){return"Delete Recipe"},
+w6(d){return"Are you sure you want to delete "+d+"?"}}
+A.aGw.prototype={
+gpp(){return"Cancel"},
+gxM(){return"Delete"}}
+A.aGA.prototype={
+gZP(){return"Edit Recipe"},
+ga1h(){return"Add New Recipe"},
+ga1f(){return"Recipe Name"},
+gYB(){return"Category"},
+gYA(){return"e.g. Soup, Main Course, Dessert"},
+ga04(){return"Image URL (optional)"},
+ga03(){return"https://..."},
+ga08(){return"Ingredients"},
+ga0n(){return"Instructions"},
+ga2P(){return"Update"},
+gNT(){return"Save"},
+ga1g(){return"Recipe name is required"},
+gYC(){return"Category is required"},
+ga09(){return"Ingredients are required"},
+ga0o(){return"Instructions are required"}}
+A.aGu.prototype={
+ge_(){return"Categories"},
+gnS(){return"No categories yet"},
+gZS(){return y.d},
+a1Z(d,e){var w=B.anw("en"),v=""+d
+return w.$3$one$other(e,v+" recipe",v+" recipes")}}
+A.aGv.prototype={
+gnS(){return"No recipes in this category"},
+gXQ(){return"Add Recipe"},
+gw7(){return"Delete Recipe"},
+w6(d){return"Are you sure you want to delete "+d+"?"}}
+A.aGz.prototype={
+ga07(){return"Ingredients"},
+ga0m(){return"Instructions"},
+ga1m(){return"Recipe not found"},
+gOm(){return"Share"},
+On(d){return d+" recipe"},
+gYY(){return"Link copied to clipboard"}}
+A.a6K.prototype={}
+var z=a.updateTypes([])
+A.aHH.prototype={
+$1(d){return A.b9k(this.a,d)},
+$S:73}
+A.aHA.prototype={
+$1$query(d){return"No results found for "+B.l(d)+"."},
+$C:"$1$query",
+$R:0,
+$D(){return{query:C.bC}},
+$S:137}
+A.aHB.prototype={
+$1$name(d){return B.l(d)+" saved!"},
+$C:"$1$name",
+$R:0,
+$D(){return{name:C.bC}},
+$S:41}
+A.aHC.prototype={
+$1$name(d){return"Are you sure you want to delete "+B.l(d)+"?"},
+$C:"$1$name",
+$R:0,
+$D(){return{name:C.bC}},
+$S:41}
+A.aHD.prototype={
+$2$count$n(d,e){var w
+this.a.gmd()
+w=B.anw("en")
+return w.$3$one$other(e,B.l(d)+" recipe",B.l(d)+" recipes")},
+$C:"$2$count$n",
+$R:0,
+$D(){return{count:C.bC,n:C.bC}},
+$S:138}
+A.aHE.prototype={
+$1$name(d){return"Are you sure you want to delete "+B.l(d)+"?"},
+$C:"$1$name",
+$R:0,
+$D(){return{name:C.bC}},
+$S:41}
+A.aHF.prototype={
+$1$name(d){return B.l(d)+" recipe"},
+$C:"$1$name",
+$R:0,
+$D(){return{name:C.bC}},
+$S:41};(function inheritance(){var w=a.mixin,v=a.inheritMany,u=a.inherit
+v(B.m,[A.a6K,A.aGy,A.aGx,A.aGB,A.aGw,A.aGA,A.aGu,A.aGv,A.aGz])
+u(A.Xd,A.a6K)
+v(B.lu,[A.aHH,A.aHA,A.aHB,A.aHC,A.aHD,A.aHE,A.aHF])
+w(A.a6K,B.iD)})()
+B.aVe(b.typeUniverse,JSON.parse('{"Xd":{"fJ":[],"iD":["fu","fJ"]}}'))
+var y={c:"Are you sure you want to delete all saved recipes?",o:"Use the search field above to find recipes",d:"You can create categories by adding new recipes"}
+var x={c:B.ac("oH<fu,fJ>")}};
+(a=>{a["3m9s/jbXffOrMF4ABrHuNFPEXks="]=a.current})($__dart_deferred_initializers__);
